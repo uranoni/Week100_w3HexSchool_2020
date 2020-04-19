d03b6a75145217adf125af434883b1c603b63040
uranoni <uranoni777@gmail.com>
2020-04-19 23:37:21 +0800

    finish week11 todo part 1

---
+++ 01_TodoList_REST/app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+var cors = require('cors');
+var app = express();
+app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/todolist01', {});
+
+//bodyParser 前端傳過來的資料要做解析
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//
+var Schema = new mongoose.Schema(
+	{
+		title: String,
+		text: String,
+		completed: {
+			type: Boolean,
+			default: false
+		}
+	},
+	{
+		timestamps: true
+	}
+);
+var Todo = mongoose.model('Todo', Schema);
+app.post('/create', async (req, res) => {
+	const { title, text } = req.body;
+	const newToda = new Todo({ title, text });
+	const result = await newToda.save();
+	res.send(result);
+});
+
+app.get('/findall', (req, res) => {
+	Todo.find()
+		.then((result) => {
+			res.send(result);
+		})
+		.catch((err) => {
+			res.status(404).send('not found');
+		});
+});
+app.listen(20201, () => {
+	console.log('http://localhost:20201');
+});
