cf038cf5ef2ff931a8c333ef36f069ffc95779a6
uranoni <uranoni777@gmail.com>
2020-10-11 22:43:20 +0800

    react mutation add book

--- 02_TodoList_GQL/client/src/App.js
+++ 02_TodoList_GQL/client/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, gql, useMutation } from '@apollo/client';
 const GET_BOOKS = gql`
 	query books {
 		books {
@@ -9,15 +9,46 @@ const GET_BOOKS = gql`
 		}
 	}
 `;
+
+const ADD_Book = gql`
+	mutation AddBook($title: String, $author: String) {
+		addBook(title: $title, author: $author) {
+			title
+			author
+		}
+	}
+`;
 function App() {
 	const { loading, error, data } = useQuery(GET_BOOKS);
+	const [AddBook, { bookdata }] = useMutation(ADD_Book);
+	const [title, setTitle] = useState('qwe');
+	const [author, setAuthor] = useState('qweqqq');
+
+	function handleTitleChange(e) {
+		setTitle(e.target.value);
+	}
+
+	function handleAuthorChange(e) {
+		setAuthor(e.target.value);
+	}
+
+	function handleAddBook(e) {
+		e.preventDefault();
+		AddBook({ variables: { title, author } });
+	}
 	if (loading) return <p>Loading...</p>;
 	if (error) return <p>Error :( </p>;
-	console.log(data);
+	// console.log(data);
 	return (
 		<div className="App">
 			<h1>{data.books.map((e) => e.title)}</h1>
 			<h2>{data.books.map((e) => e.author)}</h2>
+			請輸入書名<input type="text" value={title} onChange={handleTitleChange}></input> {title}
+			<br />
+			請輸入作者名稱<input type="text" value={author} onChange={handleAuthorChange}></input>
+			{author}
+			<br />
+			<button onClick={handleAddBook}>建立新書</button>
 		</div>
 	);
 }
--- 02_TodoList_GQL/client/src/index.js
+++ 02_TodoList_GQL/client/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 const client = new ApolloClient({
-	uri: 'http://localhost:3000',
+	uri: 'http://localhost:3000/graphql',
 	cache: new InMemoryCache()
 });
 
